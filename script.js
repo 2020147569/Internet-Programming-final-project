f1991c624c531971358b3df0ac8fd331c32ca747
2020147570 <2020147570@yonsei.ac.kr>
2021-06-04 17:59:16 +0900

    210604 17:59

---
+++ script.js
@@ -0,0 +1,17 @@
+/* input.html */
+
+function opennav () {
+    document.getElementById("sidenav").style.width = "15em";
+}
+
+function closenav () {
+    document.getElementById("sidenav").style.width = "0";
+}
+
+function addtime () {
+
+}
+
+function deletetime () {
+    
+}
