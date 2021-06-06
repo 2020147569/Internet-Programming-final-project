90d0d3ac33b9976238166f9c834028c46e7b44f1
minguni <mingguniii@yonsei.ac.kr>
2021-06-07 03:14:24 +0900

    fixerror

--- map.js
+++ map.js
@@ -75,7 +75,7 @@ var ECTimageSrc = "ECTMARKER.png";
 
   var marker = new kakao.maps.Marker({
     map: map, // 마커를 표시할 지도
-    position: positions[i].latlng, // 마커를 표시할 위치
+    position: positions[i].lating, // 마커를 표시할 위치
     title : positions[i].place_name, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
     image : markerImage
   }); // 마커 이미지 
