26344c91bddd19d7f6140f1d603e3ac6f5a4bf90
minguni <mingguniii@yonsei.ac.kr>
2021-06-07 02:44:34 +0900

    fix;;

--- map.js
+++ map.js
@@ -33,7 +33,7 @@ if (navigator.geolocation) {
     
 } else { // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
     
-    var locPosition = new kakao.maps.LatLng(37.561782, 126.936419),    
+    var locPosition = new kakao.maps.LatLng(37.561782, 126.936419);
 }
 
 var positions = [{
