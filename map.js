0226c7893071bf02ea52da313da9b319089aa183
minguni <mingguniii@yonsei.ac.kr>
2021-06-07 03:17:30 +0900

    imagesizefix

--- map.js
+++ map.js
@@ -18,7 +18,7 @@ if (navigator.geolocation) {
             lon = position.coords.longitude; // 경도
         
         var locPosition = new kakao.maps.LatLng(lat, lon); // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
-        var imageSize = new kakao.maps.Size(100, 100);
+        var imageSize = new kakao.maps.Size(20, 20);
         var nowimageSrc = new kakao.maps.MarkerImage(nowSrc, imageSize); 
 
         // 마커와 인포윈도우를 표시합니다
