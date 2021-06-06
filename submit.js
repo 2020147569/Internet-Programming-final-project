f9a9b8fcef64052d0d67800cf634a1f0480c51e7
2020147569 <80621405+2020147569@users.noreply.github.com>
2021-06-07 02:37:50 +0900

    Update submit.js

--- submit.js
+++ submit.js
@@ -3,13 +3,14 @@ $("#submitButton").click(function () {
         url: "https://pacific-garden-17851.herokuapp.com/",
         type: "POST",
         data: {
-            personnel: $("input[name=personnel").val(),
+            personnel: $("input[name=personnel]").val(),
             from: $("input[name=from]").val(),
             to: $("input[name=to]").val(),
             preference: $("input[name=preference]").val()
         },
-        success: function() {
-            console.log('data submit success');
+        success: function(data) {
+            localStorage.clear();
+            localStorage.setItem('item', data);
         }
     })
 })
