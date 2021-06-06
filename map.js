f6267ce633173c357ea343bbdd2151e6923075d6
minguni <mingguniii@yonsei.ac.kr>
2021-06-07 03:12:33 +0900

    imagefix

--- map.js
+++ map.js
@@ -1,7 +1,7 @@
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div  
     mapOption = { 
         center: new kakao.maps.LatLng(37.561782, 126.936419), // 지도의 중심좌표
-        level: 3 // 지도의 확대 레벨
+        level: 10 // 지도의 확대 레벨
     };
 
 var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
