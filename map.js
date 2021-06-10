a6cdeb43fa1b55fde54050b86ef076720a13e5c4
minguni <mingguniii@yonsei.ac.kr>
2021-06-10 18:14:30 +0900

    왜 안고쳐지지?

--- map.js
+++ map.js
@@ -95,7 +95,8 @@ else{
     map: map, // 마커를 표시할 지도
     position: poscor, // 마커를 표시할 위치
     title : positions[i].place_name, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-    image : markerImage
+    image : markerImage,
+    clickable : true
   }); // 마커 이미지 
 
   markers[i] = marker;
